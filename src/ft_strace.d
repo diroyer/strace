1bb6492e1b965759457db572a66b2ace46e0ae95
francis <richtofen@debian.backrooms.org>
2024-11-09 13:02:50 +0100

    clean

--- src/ft_strace.d
+++
@@ -1,5 +0,0 @@
-/home/richtofen/Code/strace/src/ft_strace.o: \
-  /home/richtofen/Code/strace/src/ft_strace.c \
-  /home/richtofen/Code/strace/inc/ft_strace.h
-
-/home/richtofen/Code/strace/inc/ft_strace.h:
